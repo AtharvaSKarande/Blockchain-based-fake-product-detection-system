fbffad5787b1b15e97d03047c6208543ba42c12b
Atharva Karande <atharvakarande11@gmail.com>
2023-03-28 16:27:51 +0530

    Minor file changes before working on frontend.

--- truffle/migrations/1_deploy_product_chain.js
+++ truffle/migrations/1_deploy_product_chain.js
@@ -1,9 +1,3 @@
-// const SimpleStorage = artifacts.require("SimpleStorage");
-
-// module.exports = function (deployer) {
-//   deployer.deploy(SimpleStorage);
-// };
-
 const ProductChain = artifacts.require("ProductChain");
 
 module.exports = function (deployer) {
