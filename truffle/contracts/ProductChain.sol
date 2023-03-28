7bda2370aa4e31aa067fcd453f331918a36e1e2a
Atharva Karande <atharvakarande11@gmail.com>
2023-03-28 15:50:27 +0530

    Added events to all transaction based functions.

--- truffle/contracts/ProductChain.sol
+++ truffle/contracts/ProductChain.sol
@@ -38,6 +38,7 @@ contract ProductChain {
     mapping (address => Company) private companyList;
     mapping (string => Product) private productList;
     mapping (address => User) private userList;
+    mapping (string=>string) private registeredProducts;
 
     /* Constants*/
     string LOG_CREATE_PRODUCT = "CREATE_PRODUCT";
@@ -54,6 +55,7 @@ contract ProductChain {
     /* APIs */
 
     // Company SignUp API.
+    event e_companySignUp(string, string);
     function companySignUp(address companyKey, string memory name, string memory description, bool isVerified,
         string memory emailId, string memory contactNo, string memory website) public {
         
@@ -72,11 +74,15 @@ contract ProductChain {
             companyAddresses.push(companyKey);
 
             userSignUp(companyKey, name);
+            emit e_companySignUp(HTTP_RESPONSE_CREATED, "New company created.");
+        }
+        else{
+            emit e_companySignUp(HTTP_RESPONSE_CONFLICT, "Company already exist.");
         }
     }
 
     // User SignUp API.
-    event e_userSignUp(string, address, string);
+    event e_userSignUp(string, string);
     function userSignUp(address userKey, string memory name) public {
         
         if(strEqual(userList[userKey].name, "")){
@@ -89,18 +95,23 @@ contract ProductChain {
 
             userList[userKey] = newUser;
             userAddresses.push(userKey);
-            emit e_userSignUp(HTTP_RESPONSE_CREATED, userKey, "User Created.");
+            emit e_userSignUp(HTTP_RESPONSE_CREATED, "New user Created.");
         }
         else{
-            emit e_userSignUp(HTTP_RESPONSE_CONFLICT, userKey, "User Already exist.");
+            emit e_userSignUp(HTTP_RESPONSE_CONFLICT, "User already exist.");
         }
     }
 
     // Email verified field update API. (Company)
+    event e_emailVerified(string, string);
     function emailVerified(address companyKey, bool verified) public {
         
         if(!strEqual(companyList[companyKey].name, "")){
             companyList[companyKey].isVerified = verified;
+            emit e_emailVerified(HTTP_RESPONSE_SUCCESS, "Updated the email verification status.");
+        }
+        else{
+            emit e_emailVerified(HTTP_RESPONSE_NOT_FOUND, "Company with given key does not exist.");
         }
     }
 
@@ -131,27 +142,42 @@ contract ProductChain {
     }
 
     // Register a new product API.
+    event e_registerProduct(string, string, string);
     function registerProduct(string memory productId, string memory name, address companyKey, string memory description, string memory productType) public{
 
-        bytes32 productIdHash = sha256(abi.encodePacked(productId, name, toString(abi.encodePacked(companyKey)), block.timestamp));
-        string memory uidHash = toString(abi.encodePacked(productIdHash));
+        bytes32 b_productDataHash= sha256(abi.encodePacked(productId, name, toString(abi.encodePacked(companyKey)), productType));
+        string memory productDataHash = toString(abi.encodePacked(b_productDataHash));
 
-        string[] memory logList = new string[](1);
-        string memory createLog = getLogStringFrom(0, LOG_CREATE_PRODUCT, companyKey, companyKey);
-        logList[0] = createLog;
+        if(strEqual(companyList[companyKey].name, "")){
+            emit e_registerProduct(HTTP_RESPONSE_ERROR, "Company does not exist.", "null");
+        }
+        else if(!strEqual(registeredProducts[productDataHash], "")){
+            emit e_registerProduct(HTTP_RESPONSE_CONFLICT, "Product with given credentials already exist.", "null");
+        }
+        else {
+            bytes32 b_productUID = sha256(abi.encodePacked(productId, name, toString(abi.encodePacked(companyKey)), productType, block.timestamp));
+            string memory productUID = toString(abi.encodePacked(b_productUID));
 
-        Product memory newProduct = Product({
-            productId: productId,
-            name: name,
-            companyKey: companyKey,
-            description: description,
-            productType: productType,
-            ownerKey: companyKey,
-            logs: logList
-        });
+            string[] memory logList = new string[](1);
+            string memory createLog = getLogStringFrom(0, LOG_CREATE_PRODUCT, companyKey, companyKey);
+            logList[0] = createLog;
 
-        productList[uidHash] = newProduct;
-        userList[companyKey].products.push(uidHash);
+            Product memory newProduct = Product({
+                productId: productId,
+                name: name,
+                companyKey: companyKey,
+                description: description,
+                productType: productType,
+                ownerKey: companyKey,
+                logs: logList
+            });
+
+            productList[productUID] = newProduct;
+            userList[companyKey].products.push(productUID);
+            registeredProducts[productDataHash] = productUID;
+
+            emit e_registerProduct(HTTP_RESPONSE_CREATED, "New product created.", productUID);
+        }
     }
 
     // Ownership transfer API.
